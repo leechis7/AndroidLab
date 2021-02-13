83513064d1241100023017b6682a90cad9068fbe
Chaekang Lee <leechis7@gmail.com>
2021-02-14 00:59:17 +0900

    part2_3

---
+++ part2_3/src/main/java/com/example/part2_3/Lab3_4Activity.java
@@ -0,0 +1,35 @@
+package com.example.part2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+public class Lab3_4Activity extends AppCompatActivity {
+    CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3_4);
+
+        TextView textView = findViewById(R.id.fontView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "xmas.ttf");
+        textView.setTypeface(typeface);
+
+        checkBox = findViewById(R.id.checkboox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    checkBox.setText("is Checked");
+                } else {
+                    checkBox.setText("is unChecked");
+                }
+            }
+        });
+    }
+}
